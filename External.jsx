f8171c63d253e8aa66b9f5bb12156130a6717aeb
GowthamR24 <gowthamcool.gr@gmail.com>
2017-01-03 10:13:49 +0530

    Update External.jsx

--- External.jsx
+++ External.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 export default class External extends React.Component {
    render() {
       return (
+         <div>
          <h3>
           Displaying from External.jsx File
          </h3>
+            </div>
       );
    }
 }
